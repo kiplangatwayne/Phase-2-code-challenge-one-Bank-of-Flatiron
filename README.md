843435e36333168c85f9870e862d4acadfda4716
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-07-22 21:58:05 +0300

    completed assingment

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Phase-2-code-challenge-one-Bank-of-Flatiron
+
+This is a React component named Transaction that provides a simple search and filter functionality for a list of transactions. It fetches transaction data from a specified API endpoint and allows users to search for transactions based on their descriptions.
+
+Features
+
+Fetches transaction data from the server on component mount using useEffect and fetch.
+
+Implements a search functionality that filters transactions based on the description entered by the user.
+
+Displays a table to present the transaction data with columns for Date, Category, Description, and Amount.
+
+Provides a dropdown select element to filter transactions by category
+
+Getting Started
+Clone the repository or copy the code into your React project.
+
+Make sure you have React and the necessary dependencies installed in your project.
+
+Ensure that the backend API is running and serving transaction data at http://localhost:3000/transactions.
+
+Import the Transaction component into your desired location in the application.
+
+Render the Transaction component as needed.
+
+
+User Story: Searching and Filtering Transactions
+
+As a user who wants to manage my financial transactions effectively, I want to be able to search for specific transactions based on their descriptions and filter transactions by categories. This will help me quickly find the transactions I am interested in and better understand my spending habits.
+
+
+License
+
+This project is licensed under the MIT License - replace this with the actual link to your license file.
+
--- bank-of-flatiron/src/Transaction.js
+++ bank-of-flatiron/src/Transaction.js
@@ -1,57 +1,89 @@
 import React, { useState, useEffect } from 'react';
 
-function Transaction(transaction) {
+function Transaction() {
   const [transactions, setTransactions] = useState([]);
   const [search, setSearch] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:3000/transactions')
-      .then(response => response.json())
-      .then(data => setTransactions(data))
+    fetch('https://api.npoint.io/90ec8e113de724e8ec7d')
+      .then(resp => {
+        if (!resp.ok) {
+          throw new Error('Failed to fetch data from the server.');
+        }
+        return resp.json();
+      })
+      .then(data => {
+        setTransactions(data.transactions);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
   const handleSearchChange = event => {
     setSearch(event.target.value);
   };
 
+  const handleCategoryChange = event => {
+    setSelectedCategory(event.target.value);
+  };
+
   const filteredTransactions = transactions.filter(transaction =>
-    transaction.description.search(search) > -1
+    transaction.description.toLowerCase().includes(search.toLowerCase()) &&
+    (selectedCategory === '' || transaction.category === selectedCategory)
   );
 
   return (
     <>
-      <div id='div'>
-        <input type='text' placeholder='Search...' onChange={handleSearchChange} id='search'/>
-        <select>
-          <option>Income</option>
-          <option>Food</option>
-          <option>Fashion</option>
-          <option>Gift</option>
-          <option>Transportation</option>
-          <option>Entertainment</option>
-          <option>Housing</option>
-        </select>
-        <table>
-          <thead>
-            <tr>
-              <th id='dat'>Date</th>
-              <th id='dat'>Category</th>
-              <th id='des'>Description</th>
-              <th id='amnt'>Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredTransactions.map(transaction => (
-              <tr key={transaction.id}>
-                <td>{transaction.date}</td>
-                <td>{transaction.category}</td>
-                <td>{transaction.description}</td>
-                <td>{transaction.amount}</td>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <div id='div'>
+          <input
+            type='text'
+            placeholder='Search...'
+            onChange={handleSearchChange}
+            id='search'
+          />
+          <select onChange={handleCategoryChange} value={selectedCategory}>
+            <option value=''>All</option>
+            <option value='Income'>Income</option>
+            <option value='Food'>Food</option>
+            <option value='Fashion'>Fashion</option>
+            <option value='Gift'>Gift</option>
+            <option value='Transportation'>Transportation</option>
+            <option value='Entertainment'>Entertainment</option>
+            <option value='Housing'>Housing</option>
+          </select>
+          <table>
+            <thead>
+              <tr>
+                <th id='dat'>Date</th>
+                <th id='dat'>Category</th>
+                <th id='des'>Description</th>
+                <th id='amnt'>Amount</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {filteredTransactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td>{transaction.date}</td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.description}</td>
+                  <td>{transaction.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </>
   );
 }
